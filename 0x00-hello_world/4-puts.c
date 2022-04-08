22dd17328c3f9ac449c1d112dd1e9e35dcfa4be3
edwindevs <edmbogo@outlook.com>
2022-04-07 17:17:22 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*C programme that prints a string
+*using function puts
+*program returns 0 after output
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
