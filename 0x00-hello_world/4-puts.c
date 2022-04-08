d1a40d8b6bea4b3f62373f65aa780fc62d37c8ec
edwindevs <edmbogo@outlook.com>
2022-04-07 18:09:22 -0700

    4 Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main: Print statement
- * Return: 0 after output
+ * main - The containing function of this programme
+ * Return: this gives 0 if sucessful
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
