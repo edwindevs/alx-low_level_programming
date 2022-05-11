13b06186ed5a5c1db7708c17e456f1ae38fd23da
edwindevs <edmbogo@outlook.com>
2022-05-11 01:55:32 +0000

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,147 +1,125 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+#include <string.h>
+/*array of 100000000 (4GB) needs a capable system*/
+/*provided space on the vagrant doesn't cut it*/
+#define MAX 10000000
+
+char *multiply(char [], char []);
+
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
- *
- * Return: void
- */
-void _print(char *str, int l)
-{
-	int i, j;
-
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	free(str);
-}
-
-/**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- *
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
-}
-/**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- *
- * Return: 0 if digits, 1 if not
- */
-int check_for_digits(char **av)
-{
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * init - initializes a string
- * @str: sting to initialize
- * @l: length of strinf
- *
- * Return: void
- */
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: argument vector
- *
- * Return: zero, or exit status of 98 if failure
+ * main - multiply 2 arg of num > 0
+ * @argc: param count
+ * @argv: param arr
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
+	unsigned long mul;
+	int i, j;
+	char *c;
 
-	if (argc != 3 || check_for_digits(argv))
+	if (argc != 3)
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+		printf("Error\n");
 		exit(98);
 	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
+
+	for (i = 1; i < argc; i++)
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+
+	}
+
+	c = multiply(argv[1], argv[2]);
+
+	for (mul = 1; c[mul] != '\0'; mul++)
+		_putchar(c[mul]);
+	_putchar('\n');
+
+	return (0);
+}
+
+/**
+ * multiply - multiply the inputs
+ * @a: param
+ * @b: param
+ * Return: char pointer
+ */
+
+char *multiply(char a[], char b[])
+{
+	static char mul[MAX];
+	char c[MAX];
+	char temp[MAX];
+	int la, lb;
+	int i, j, k = 0, x = 0, y;
+	long int r = 0;
+	long sum = 0;
+
+	la = strlen(a) - 1;
+	lb = strlen(b) - 1;
+
+	for (i = 0; i <= la; i++)
+		a[i] = a[i] - 48;
+
+	for (i = 0; i <= lb; i++)
+		b[i] = b[i] - 48;
+
+	for (i = lb; i >= 0; i--)
+	{
+		r = 0;
+
+		for (j = la; j >= 0; j--)
+		{
+			temp[k++] = (b[i] * a[j] + r) % 10;
+			r = (b[i] * a[j] + r) / 10;
+		}
+		temp[k++] = r;
+		x++;
+
+		for (y = 0; y < x; y++)
+		{
+			temp[k++] = 0;
 		}
 	}
-	_print(a, ln - 1);
-	return (0);
+
+	k = 0;
+	r = 0;
+
+	for (i = 0; i < la + lb + 2; i++)
+	{
+		sum = 0;
+		y = 0;
+
+		for (j = 1; j <= lb + 1; j++)
+		{
+			if (i <= la + j)
+			{
+				sum = sum + temp[y + i];
+			}
+			y += j + la + 1;
+		}
+		c[k++] = (sum + r) % 10;
+		r = (sum + r) / 10;
+	}
+	c[k] = r;
+	j = 0;
+	for (i = k - 1; i >= 0; i--)
+	{
+		mul[j++] = c[i] + 48;
+	}
+	mul[j] = '\0';
+
+	return (mul);
 }
